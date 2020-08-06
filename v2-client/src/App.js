16f9c6a506939a90e3cd2334fdbcdc8948326b9d
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-06 19:57:12 +0200

    added packages

--- v2-client/src/App.js
+++ v2-client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react"
 
 function App() {
-	return <div className="App">"hi"</div>
+	return (
+		<>
+			<div className="intro-half-one"></div>
+			<div className="intro-half-two"></div>
+		</>
+	)
 }
 
 export default App
