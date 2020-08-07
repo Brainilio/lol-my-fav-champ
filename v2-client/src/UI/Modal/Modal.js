4099e72d1eb8f15ec888056fe54fca357b066659
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-07 17:08:19 +0200

    pulled data and info from each champion

--- v2-client/src/UI/Modal/Modal.js
+++ v2-client/src/UI/Modal/Modal.js
@@ -38,19 +38,56 @@ const Modal = (props) => {
 			.catch((error) => console.log(error))
 	}, [])
 
+	let officialData = null
+	if (riotApiData) {
+		for (const key in riotApiData) {
+			const championInfo = riotApiData[key]
+
+			officialData = (
+				<div className="official-data">
+					<h1>Official data found on your champion:</h1>
+					<span>{championInfo.name}</span>
+					<span>{championInfo.title}</span>
+					<p>{championInfo.lore}</p>
+					<div>
+						<span>Ally tips:</span>
+						<p>{championInfo.allytips.map((tip) => tip + " ")}</p>
+						<span>Enemy tips:</span>
+						<p>{championInfo.enemytips.map((tip) => tip + " ")}</p>
+					</div>
+				</div>
+			)
+		}
+	}
+
 	return (
-		<div className="Modal" onClick={props.clicked}>
-			<span>X</span>
+		<div className="Modal">
+			<span onClick={props.clicked} className="close-modal">
+				X
+			</span>
 			{props.children}
 			{champ ? (
-				<p>
-					{champ.name} & {champ.type} & {champ.lane}{" "}
-				</p>
+				<div className="my-information">
+					<h1>My information</h1>
+					<p>
+						{champ.name} & {champ.type} & {champ.lane}{" "}
+					</p>
+					<span>Edit now</span>
+				</div>
 			) : (
 				<Spinner />
 			)}
+
+			{/* Official information */}
+			{officialData}
+			{/* Image */}
 			{champImage ? (
-				<img src={champImage} alt="Champion" />
+				<img
+					className="champion-image"
+					draggable="false"
+					src={champImage}
+					alt="Champion"
+				/>
 			) : (
 				<p>Loading.div..</p>
 			)}
