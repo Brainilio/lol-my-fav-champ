9ea9ffc6c0d4e4b4716b4228b881aa757a5b3560
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-20 19:27:57 +0200

    made a form for adding champs

--- v2-client/src/components/AddChamp/AddChamp.js
+++ v2-client/src/components/AddChamp/AddChamp.js
@@ -1,7 +1,14 @@
 import React from "react"
 
 const AddChamp = (props) => {
-	return <form></form>
+	return (
+		<form>
+			<input type="text" name="name" value />
+			<input type="text" name="type" value />
+			<input type="text" name="lane" value />
+			<input type="number" name="cost" value />
+		</form>
+	)
 }
 
 export default AddChamp
