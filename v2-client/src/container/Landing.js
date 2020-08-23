709447314b27259c670e5e3a20eca334f423b127
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-23 14:22:28 +0200

    LANDING PAGE ODNE

--- v2-client/src/container/Landing.js
+++ v2-client/src/container/Landing.js
@@ -1,7 +1,23 @@
 import React from "react"
+import background from "../assets/landingbackground.png"
+import phone from "../assets/phone.png"
+import "./Landing.scss"
 
 const Landing = () => {
-	return <div>Landing</div>
+	return (
+		<section className="Landing">
+			<img classname="background-img" src={background} alt="backgrround" />
+			<div className="text">
+				<h1>LOL: MY FAVORITE CHAMPION</h1>
+				<p>
+					Manage your favorite League of Legends champions in your own
+					personalized dashboard. Now with real data from Riot Games!{" "}
+				</p>
+				<button>JOIN NOW</button>
+			</div>
+			<img className="phone" src={phone} />
+		</section>
+	)
 }
 
 export default Landing
