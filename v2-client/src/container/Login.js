c70843e848c06fbeecc386049404adff6b6121ad
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-23 14:49:48 +0200

    login page done

--- v2-client/src/container/Login.js
+++ v2-client/src/container/Login.js
@@ -1,8 +1,30 @@
 import React from "react"
 import "./Login.scss"
+import background from "../assets/loginbackground.png"
+import { NavLink } from "react-router-dom"
 
 const Login = () => {
-	return <div></div>
+	return (
+		<section className="Login">
+			<img src={background} alt="" />
+			<form className="login-form">
+				<h1>LOGIN</h1>
+				<label htmlFor="email">Email</label>
+				<input type="text" />
+				<label htmlFor="password">Password</label>
+				<input type="password" />
+				<button>
+					<NavLink
+						to="/dashboard"
+						style={{ textDecoration: "none", color: "#f9e09c" }}
+					>
+						LOGIN
+					</NavLink>
+				</button>
+				<span>OR SIGN UP</span>
+			</form>
+		</section>
+	)
 }
 
 export default Login
